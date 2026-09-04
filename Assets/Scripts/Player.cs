bab81a4b9acb697fa98e3cf5a520072634dff278
Clorax <galvezjohndominick@gmail.com>
2026-09-04 14:42:51 +0800

    nmbnmgh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,12 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
-
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private InputAction playerControls;
     [SerializeField] private Vector2 moveDirection;
@@ -17,6 +21,13 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = moveDirection * moveSpeed;
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && playerControls.triggered)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     private void OnEnable()
